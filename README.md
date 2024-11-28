5e6fcdafede90eea295abb6f6f47400ca087b8db
ahammednibras8 <ahammednibras@proton.me>
2024-11-28 14:37:58 +0530

    initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mindful_habit_builder_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mindful_habit_builder_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mindful_habit_builder_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
