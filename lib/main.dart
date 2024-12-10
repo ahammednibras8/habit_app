086db1129999a9d233845e3144377b14bc7245bb
Aysha Riya <aysha@Ayshas-MacBook-Air.local>
2024-12-10 11:25:32 +0530

    Todo: - Gender selction Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindful_habit_builder_app/views/auth/gender_selection_screen.dart';
 import 'package:mindful_habit_builder_app/views/onboarding/splash_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: GenderSelectionScreen(),
     );
   }
 }
